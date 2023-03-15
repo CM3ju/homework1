54d6f08ee5750b068eb3a011ed6407bba188ed4b
kanade <6795360zj@gmail.com>
2023-03-15 16:41:06 +0300

    0.1

--- script.py
+++ script.py
@@ -1,3 +1,31 @@
-import request
+import requests
 import pandas
 
+URL = 'https://pypi.org/search/'
+
+
+class Web:
+    def __init__(self, url=URL):
+        self.url = url
+        self.status = None
+
+    def get_status(self):
+        status = requests.request('GET', self.url)
+        self.status = status
+        status = int(str(status).split(" ")[1][1:-2])
+        if status == 200:
+            print('request successful')
+            print(f'url is: {self.url}')
+        else:
+            print(f'request failed your response is: {status}')
+        return self.status
+
+    def get_content(self):
+        content = requests.get(self.url)
+        return content.text
+
+
+
+
+a = Web().get_content()
+print(a)
