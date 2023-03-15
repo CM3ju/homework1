a52ffba59d560967d043ce0a97c8ea0cbc30638f
kanade <6795360zj@gmail.com>
2023-03-15 17:58:44 +0300

    0.2

--- script.py
+++ script.py
@@ -1,13 +1,16 @@
 import requests
-import pandas
+from lxml import etree
 
-URL = 'https://pypi.org/search/'
+URL = 'https://pypi.org/search/?c=Programming+Language+%3A%3A+Python+%3A%3A+3'
 
 
 class Web:
-    def __init__(self, url=URL):
+    def __init__(self, url=URL, save_dir='./'):
         self.url = url
         self.status = None
+        self.encoding = None
+        self.save_dir = save_dir
+        self.content = None
 
     def get_status(self):
         status = requests.request('GET', self.url)
@@ -22,10 +25,17 @@ class Web:
 
     def get_content(self):
         content = requests.get(self.url)
-        return content.text
+        with open('main_page.html', 'w', encoding='utf-8') as f:
+            f.write(content.text)
+        self.content = content.text
 
+    def decode(self):
+        tree = etree.HTML(self.content)
+        list_li = tree.xpath('//ul[@class="unstyled"]/li')
+        for li in list_li:
+            list_herl = li.xpath('./a[@href]')[0]
+        print()
 
-
-
-a = Web().get_content()
-print(a)
+a = Web()
+a.get_content()
+a.decode()
